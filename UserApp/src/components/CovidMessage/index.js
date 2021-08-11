0a5fce666fa0cbad450892cd5d53f6235ef177e9
Jessica Sun <jessicasun@Jessicas-Air.attlocal.net>
2021-08-10 23:29:25 -0500

    Update travel alert message

--- UserApp/src/components/CovidMessage/index.js
+++ UserApp/src/components/CovidMessage/index.js
@@ -8,7 +8,7 @@ const CovidMessage = (props) => {
     <View style={styles.container}>
       <Text style={styles.title}>Travel only if necessary</Text>
       <Text style={styles.text}>
-        Upgrading this package often requires the font files linked to your projects to be updated as well. If the automatic linking works for you, running this again should update the fonts.
+        Covid-19 alert
       </Text>
       <Text style={styles.learnMore}>Learn more</Text>
     </View>
